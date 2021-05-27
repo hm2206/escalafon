c4ac0f453209899d5cdfcd92efcbfd2ecb938310
Hans <twd2206@gmail.com>
2021-05-27 07:29:32 -0500

    fix sync assistance

--- app/Jobs/SyncClock.js
+++ app/Jobs/SyncClock.js
@@ -9,6 +9,7 @@ const ZKLib = require('node-zklib');
 const uid = require('uid');
 const Drive = use('Drive');
 const { getSystemKey } = require('../Services/tools');
+const { authentication } = require('../Services/apis');
 
 class SyncClock {
   // If this getter isn't provided, it will default to 1.
@@ -34,14 +35,17 @@ class SyncClock {
   }
 
   async syncAssistance (datos) {
-    if (!datos.length) return;
+    if (!datos.length) {
+      this.logs = [];
+      return;
+    }
     // generar slug
     this.slug = uid(10);
     // guardar logs temporal
     this.pathRelative = `clock/sync/assistance_${this.slug}.json`;
     await Drive.put(this.pathRelative, Buffer.from(JSON.stringify(datos)));
     // setting logs
-    this.logs = collect(datos).sortBy('recordTime');
+    this.logs = datos;
   }
 
   async saveAssistance () {
@@ -49,11 +53,10 @@ class SyncClock {
     for (let log of this.logs) {
       let current_date = moment(log.recordTime).format('YYYY-MM-DD');
       let current_time = moment(log.recordTime).format('HH:mm:ss');
-      let exists_config_assistance = this.config_assistances.where('date', current_date).first();
-      let config_assistance;
-      if (exists_config_assistance) config_assistance = exists_config_assistance;
-      else {
+      let config_assistance = this.config_assistances.where('date', current_date).first() || null;
+      if (!config_assistance) {
         config_assistance = await ConfigAssistance.query()
+          .where('entity_id', this.clock.entity_id)
           .where('date', current_date)
           .first();
         // agregar en caché
@@ -70,17 +73,19 @@ class SyncClock {
         .where('work_id', work.id)
         .orderBy('record_time', 'DESC')
         .first();
-      if (!last_assistance) last_assistance = await this.storage.where('work_id', work.id)
-        .where('config_assistance_id', config_assistance.id)
-        .sortByDesc('record_time', 'DESC')
-        .first();
       // obtener tipo de assistencia
       let status = 'ENTRY';
       if (last_assistance) {
-        if (last_assistance.record_time == current_time) continue;
+        // ya existe el record time
+        let exists_record_time = await Assistance.query()
+          .where('config_assistance_id', config_assistance.id)
+          .where('work_id', work.id)
+          .where('record_time', current_time)
+          .getCount('id');
+        if (exists_record_time) continue;
         status = last_assistance.status == 'ENTRY' ? 'EXIT' : 'ENTRY';
       }
-      // guardar datos locales en el storage
+      // preparar datos 
       let payload = {
         config_assistance_id: config_assistance.id,
         work_id: work.id,
@@ -88,17 +93,16 @@ class SyncClock {
         record_time: current_time,
         status
       }
-      // guardar en el storage
-      this.storage.push(payload);
+      // guardar en el assistance
+      await Assistance.create(payload);
+      await this.storage.push(payload);
     }
-    // guardar en la db
-    await Assistance.createMany(this.storage.toArray());
     // eliminar json
     let exists = await Drive.exists(this.pathRelative);
     if (exists) await Drive.delete(this.pathRelative);
   }
 
-  async sendNotification () {
+  async sendNotification (title, description) {
     // options
     let options = {
         headers: {
@@ -111,19 +115,34 @@ class SyncClock {
     // payload
     let payload = {
         receive_id: this.auth.id,
-        title: `La sincronización del reloj "${this.clock.name}" se realizó correctamente`,
-        description: `Ahora todos los datos encontrados fuerón sincronizados con el sistema`,
+        title,
+        description,
         method: this.method,
         object_type: "App/Models/Assistance",
         object_id: 0
     };
     // request
-    authentication.post(`auth/notification`, payload, options)
+    let { success, message } = await authentication.post(`auth/notification`, payload, options)
     .then(res => res.data)
     .catch(err => ({ success: false, message: err.message }));
   }
 
-  // This is where the work is done.
+  async sendNotificationSuccess () {
+    await this.sendNotification(
+      `La sincronización de asistencia "${this.clock.name}" se realizó correctamente`,
+      `Se encontrarón ${this.storage.count()} regístros de asistanecia, la cual se sincronizó con el sistema`
+    )
+    console.log('successfull');
+  }
+
+  async sendNotificationError (error) {
+    await this.sendNotification(
+      `La sincronización falló`,
+      `No se pudo procesar la sincronización de asistencia, intente de nuevo!!!`
+    )
+    console.log('error', error.message);
+  }
+
   async handle (data) {
     let { clocks, auth, app, method } = data;
 
@@ -133,19 +152,25 @@ class SyncClock {
     this.config_assistances = collect([]);
     this.storage = collect([]);
 
-    for (let clock of clocks) {
-      this.clock = clock;
-      this.syncConnect = await this.connect();
-      let logs = await this.syncConnect.getAttendances();
-      this.syncConnect.disconnect();
-      // sincronizar los datos del reloj con el sistema de escalafón
-      await this.syncAssistance(logs.data);
-      // guardar datos
-      await this.saveAssistance();
+    // procesae
+    try {
+      for (let clock of clocks) {
+        this.clock = clock;
+        this.syncConnect = await this.connect();
+        let logs = await this.syncConnect.getAttendances();
+        this.syncConnect.disconnect();
+        // sincronizar los datos del reloj con el sistema de escalafón
+        await this.syncAssistance(logs.data);
+        // guardar datos
+        await this.saveAssistance();
+      }
+      // enviar notificación
+      await this.sendNotificationSuccess();
+    } catch (error) {
+      await this.sendNotificationError(error);
     }
-    // enviar notification
-    await  this.sendNotification();
   }
+  
 }
 
 module.exports = SyncClock
