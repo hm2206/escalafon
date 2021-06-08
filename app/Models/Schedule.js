e64ce72bbcd958232033b27bcd67bde3ffa604f6
Hans <twd2206@gmail.com>
2021-06-08 05:36:47 -0500

    fix format date => schedules

--- app/Models/Schedule.js
+++ app/Models/Schedule.js
@@ -3,9 +3,13 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 const DB = use('Database');
+const moment = require('moment');
 
 class Schedule extends Model {
 
+    getDate (value) {
+        return moment(value).format('YYYY-MM-DD');
+    }
 
 }
 
