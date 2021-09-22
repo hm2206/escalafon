44bc091c02a9c9b3bc07a082914934dcf6451324
Hans <twd2206@gmail.com>
2021-09-22 18:04:24 -0500

    add xlsx

--- app/Controllers/Http/ReportController.js
+++ app/Controllers/Http/ReportController.js
@@ -5,12 +5,13 @@ const ReportGeneralBuilder = require('../../Helpers/ReportGeneralBuilder');
 class ReportController {
 
     async general({ request, response }) {
+        let type = request.input('type', 'pdf');
         let filters = request.only(['cargo_id', 'type_categoria_id']);
         let authentication = request.api_authentication;
-        const reportGeneralBuilder =  new ReportGeneralBuilder(authentication, filters);
-        const result = await reportGeneralBuilder.render();
-        response.header('Content-Type', 'application/pdf')
-        return response.send(result);
+        const reportGeneralBuilder =  new ReportGeneralBuilder(authentication, filters, type);
+        const builder = await reportGeneralBuilder.render();
+        response.type(builder.header);
+        return response.send(builder.result);
     }
 
 }
--- app/Helpers/ReportGeneralBuilder.js
+++ app/Helpers/ReportGeneralBuilder.js
@@ -3,7 +3,9 @@
 const View = use('View');
 const { default: collect } = require('collect.js');
 const htmlToPdf = require('html-pdf-node');
+const moment = require('moment');
 const Work = use('App/Models/Work');
+const xlsx = require('node-xlsx');
 
 class ReportGeneralBuilder {
 
@@ -13,16 +15,33 @@ class ReportGeneralBuilder {
         type_categoria_id: ""
     }
 
+    result = null
+
+    type = "pdf"
+
+    allowType = {
+        pdf: {
+            handle: this.formatPDF,
+            header: 'application/pdf'
+        },
+        excel: {
+            handle: this.formatExcel,
+            header: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        },
+    }
+
     options = {
         format: 'A4'
     }
 
     works = []
 
-    constructor(authentication = {}, filters = this.filters) {
+
+    constructor(authentication = {}, filters = this.filters, type = 'pdf') {
         this.authentication = authentication;
         this.filters = Object.assign(this.filters, filters);
         this.people = collect([]);
+        this.type = type;
     }
 
     async getWorks() {
@@ -67,7 +86,12 @@ class ReportGeneralBuilder {
 
     async settingWorks() {
         for(let work of this.works) {
-            work.person = await this.people.where('id', work.person_id).first() || {}
+            let person = await this.people.where('id', work.person_id).first() || {}
+            if (person.id) {
+                person.date_of_birth = moment(`${person.date_of_birth}`, 'YYYY-MM-DD').format('DD/MM/YYYY');
+            }
+
+            work.person = person;
         }
     }
 
@@ -75,6 +99,38 @@ class ReportGeneralBuilder {
         return {
             works: this.works,
         }
+    }
+
+    async formatPDF(that, datos = []) {
+        const html = View.render('report/general', datos);
+        const file = { content: html }
+        return await htmlToPdf.generatePdf(file, that.options);
+    }
+
+    async formatExcel(that, datos = {}) {
+        let headers = ["N°", "Apellidos y Nombres", "Tipo", "Document", "Sexo", "Fecha de Nacimiento", "Edad", "Correo", "Teléfono"];
+        let content = [];
+        let works = [...datos.works];
+        // mapping
+        works.map((w, index) => {
+            content.push([
+                index + 1,
+                `${w.person.fullname || ''}`.toUpperCase(),
+                `${w.person.document_type.name || ''}`.toUpperCase(),
+                `${w.person.document_number || ''}`,
+                `${w.person.gender || ''}`,
+                `${w.person.date_of_birth || ''}`,
+                `${w.person.edad || ''}`,
+                `${w.person.email_contact || ''}`,
+                `${w.person.phone || ''}`
+            ]);
+            // data
+            return w;
+        });
+        // response
+        let data = [headers, ...content];
+        let result = await xlsx.build([{ name: 'reporte-general', data }])
+        return result;
     }
 
     async render() {
@@ -86,10 +142,16 @@ class ReportGeneralBuilder {
         await this.settingWorks();
         // render
         const datos = this.dataRender();
-        const html = View.render('report/general', datos);
-        const file = { content: html }
-        return await htmlToPdf.generatePdf(file, this.options);
-    }
+        // render type
+        let handle = this.allowType[this.type];
+        let that = this;
+        if (typeof handle.handle != 'function') throw new Error("No se pudó generar el reporte");
+        let result = await handle.handle(that, datos);
+        return {
+            result,
+            header: handle.header
+        };
+    }   
 
 }
 
